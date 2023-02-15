b5d26e4a683e1ed5bd90a0059cbb8bdc8713e319
c0nfid <beiov2004.mail@gmail.com>
2023-02-15 23:28:06 +0300

    Add Nodes classes

---
+++ Node.py
@@ -0,0 +1,38 @@
+from Lex import Token
+
+class Node:
+    None
+
+
+class StatementsNode(Node):
+    codeStrings = [] #Nodes
+
+    def addNode(self, node: Token):
+        self.codeStrings.append(node)
+
+
+class VariableNode(Node):
+
+    def __init__(self, variable: Token):
+        self.variable = variable
+
+
+class NumberNode(Node):
+
+    def __init__(self, number: Token):
+        self.number = number
+
+
+class BinOperationNode(Node):
+
+    def __init__(self, operator: Token, leftNode: Node, rightNode: Token):
+        self.operator = operator
+        self.leftNode = leftNode
+        self.rightNode = rightNode
+
+
+class UnarOperationNode(Node):
+
+    def __init__(self, operator: Token, operand: Node):
+        self.operator = operator
+        self.operand = operand
